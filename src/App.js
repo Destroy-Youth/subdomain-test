6fbc1324a0af1a1e0efd030c7ff327117a5aba73
Ivan Herrera Equihua <luis.herrera@mymoons.mx>
2022-06-16 16:02:20 -0500

    iframe test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+
+const domains = ["http://localhost:3000", "http://localhost:3001"];
+
+function messageHandler(event) {
+  if (!domains.includes(event.origin)) return;
+  const { action, key, value } = event.data;
+  if (action == "get") {
+    event.source.postMessage(
+      {
+        action: "returnData",
+        key,
+        value: localStorage.getItem("session"),
+      },
+      "*"
+    );
+  }
+}
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("message", messageHandler, false);
+    localStorage.setItem("session", "session123");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="iframe1 host">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
